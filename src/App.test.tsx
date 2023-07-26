1e4e038e0924e5afdcba4eb7f5210087344c3da2
Tyra Krehbiel <tyra.krehbiel98@gmail.com>
2023-07-26 19:52:05 -0400

    fixed failing test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-jest.mock('./features/temp-page/TempPage', () =>
-    () => <div data-testid='temp-page' />
+jest.mock('./features/home/Home', () =>
+    () => <div data-testid='home' />
 );
 
 describe('App', () => {
     it('renders home page on load', () => {
         render(<App />);
-        expect(screen.getByTestId('temp-page')).toBeInTheDocument();
+        expect(screen.getByTestId('home')).toBeInTheDocument();
     });
 });
