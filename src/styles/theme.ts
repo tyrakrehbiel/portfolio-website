3d7fb2b20766f60089cf9df08e4e5fd1ae0cc84d
Tyra Krehbiel <tyra.krehbiel98@gmail.com>
2023-07-26 20:28:57 -0400

    changes

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,58 +2,58 @@ import { createTheme } from '@mui/material/styles';
 import tm from './2-utils/_theme.module.scss';
 
 const theme = createTheme({
-    palette: {
-        mode: 'light',
-        primary: {
-            main: tm.palette_primary_main,
-            contrastText: tm.palette_primary_contrastText,
-        },
-        secondary: {
-            main: tm.palette_secondary_main,
-            contrastText: tm.palette_secondary_contrastText,
-        },
-        error: {
-            main: tm.palette_error_main,
-            contrastText: tm.palette_error_contrastText,
-        },
-        warning: {
-            main: tm.palette_warning_main,
-            contrastText: tm.palette_warning_contrastText,
-        },
-        info: {
-            main: tm.palette_info_main,
-            contrastText: tm.palette_info_contrastText,
-        },
-        success: {
-            main: tm.palette_success_main,
-            contrastText: tm.palette_success_contrastText,
-        },
-        text: {
-            primary: tm.palette_text_primary,
-            secondary: tm.palette_text_secondary,
-            disabled: tm.palette_text_disabled,
-        },
-        grey: {
-            50: tm.common_palette_grey_50,
-            100: tm.common_palette_grey_100,
-            200: tm.common_palette_grey_200,
-            300: tm.common_palette_grey_300,
-            400: tm.common_palette_grey_400,
-            500: tm.common_palette_grey_500,
-            600: tm.common_palette_grey_600,
-            700: tm.common_palette_grey_700,
-            800: tm.common_palette_grey_800,
-            900: tm.common_palette_grey_900,
-            A100: tm.common_palette_grey_A100,
-            A200: tm.common_palette_grey_A200,
-            A400: tm.common_palette_grey_A400,
-            A700: tm.common_palette_grey_A700,
-        },
-        background: {
-            default: tm.palette_background_default,
-            paper: tm.palette_background_paper,
-        }
-    },
+    // palette: {
+    //     mode: 'light',
+    //     primary: {
+    //         main: tm.palette_primary_main,
+    //         contrastText: tm.palette_primary_contrastText,
+    //     },
+    //     secondary: {
+    //         main: tm.palette_secondary_main,
+    //         contrastText: tm.palette_secondary_contrastText,
+    //     },
+    //     error: {
+    //         main: tm.palette_error_main,
+    //         contrastText: tm.palette_error_contrastText,
+    //     },
+    //     warning: {
+    //         main: tm.palette_warning_main,
+    //         contrastText: tm.palette_warning_contrastText,
+    //     },
+    //     info: {
+    //         main: tm.palette_info_main,
+    //         contrastText: tm.palette_info_contrastText,
+    //     },
+    //     success: {
+    //         main: tm.palette_success_main,
+    //         contrastText: tm.palette_success_contrastText,
+    //     },
+    //     text: {
+    //         primary: tm.palette_text_primary,
+    //         secondary: tm.palette_text_secondary,
+    //         disabled: tm.palette_text_disabled,
+    //     },
+    //     grey: {
+    //         50: tm.common_palette_grey_50,
+    //         100: tm.common_palette_grey_100,
+    //         200: tm.common_palette_grey_200,
+    //         300: tm.common_palette_grey_300,
+    //         400: tm.common_palette_grey_400,
+    //         500: tm.common_palette_grey_500,
+    //         600: tm.common_palette_grey_600,
+    //         700: tm.common_palette_grey_700,
+    //         800: tm.common_palette_grey_800,
+    //         900: tm.common_palette_grey_900,
+    //         A100: tm.common_palette_grey_A100,
+    //         A200: tm.common_palette_grey_A200,
+    //         A400: tm.common_palette_grey_A400,
+    //         A700: tm.common_palette_grey_A700,
+    //     },
+    //     background: {
+    //         default: tm.palette_background_default,
+    //         paper: tm.palette_background_paper,
+    //     }
+    // },
     breakpoints: {
         values: {
             xs: parseInt(tm.breakpoints_xs),
