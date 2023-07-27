b2acb6f9d51978b19db32e91ff1e898cf1a04338
Tyra Krehbiel <tyra.krehbiel98@gmail.com>
2023-07-26 22:35:12 -0400

    modified typography

--- src/common/header/Header.tsx
+++ src/common/header/Header.tsx
@@ -1,29 +1,39 @@
 import * as React from 'react';
 import { AppBar, Toolbar, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const Header: React.FC = () => {    
+const Header: React.FC = () => {
+
+    const navigate = useNavigate();
+
+    const downloadResume = () => {
+        console.log('downloading resume');
+    };
+
+    const routes = [
+        {
+            label: 'About',
+            onClick: () => navigate('/about')
+        }, {
+            label: 'Portfolio',
+            onClick: () => navigate('/portfolio')
+        }, {
+            label: 'Resume',
+            onClick: downloadResume
+        }, {
+            label: 'Contact',
+            onClick: () => navigate('/contact')
+        }
+    ];
+
     return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h2">
-                        Tyra Krehbiel
-                    </Typography>
-                    <Button color="inherit">
-                        About
-                    </Button>
-                    <Button>
-                        Projects
-                    </Button>
-                    <Button>
-                        Art
-                    </Button>
-                    <Button>
-                        Contact
-                    </Button>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <AppBar>
+            <Toolbar variant='dense'>
+                <Typography variant='h5' className='header-title'>
+                    Tyra Krehbiel
+                </Typography>
+            </Toolbar>
+        </AppBar>
     );
 };
 
