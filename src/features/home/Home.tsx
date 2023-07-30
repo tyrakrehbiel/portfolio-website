9f9d2f53bea51b8ed07799f47e5d91c6505f8051
Tyra Krehbiel <tyra.krehbiel98@gmail.com>
2023-07-30 17:43:55 -0400

    updated hoome

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
 import { Button, Grid, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const content = {
+const content1 = {
+    name: 'Tyra Krehbiel',
+    title: ['Full Stack Software Developer,','UI/UX Designer,','& Artist'],
+    cta: 'Hire Me'
+};
+
+const content2 = {
     greeting: 'Hello and welcome!',
     intro: 'I\'m Tyra, a Fullstack Software Developer with a flair for UI/UX design and a passion for artistry.',
     primary: 'In this digital portfolio, you\'ll find the perfect blend of technology and creativity. ' + 
@@ -12,26 +19,31 @@ const content = {
 };
 
 const Home: React.FC = () => {
+
+    const navigate = useNavigate();
+
     return (
         <Grid container className='home'>
-            <Grid item container xs={12} className='content-section-1'>
+            <Grid item container xs={12} className='content-section-1' spacing={2}>
                 <Grid item xs={12} md={6}>
-                    <Typography variant='h1'>Tyra Krehbiel</Typography>
-                    <Typography variant='h2'>Full Stack Software Developer,</Typography>
-                    <Typography variant='h2'>UI/UX Designer,</Typography>
-                    <Typography variant='h2'>& Artist</Typography>
-                    <Button variant='contained'>Hire Me</Button>
+                    <Typography variant='h1' className='title'>{content1.name}</Typography>
+                    {content1.title.map((t,i) =>
+                        <Typography variant='h2' key={`title-${i}`}>{t}</Typography>)
+                    }
+                    <Button variant='contained' color='secondary' size='large' className='cta-button' onClick={() => navigate('/contact')}>{content1.cta}</Button>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <div style={{ height: '500px', backgroundColor: 'gray' }} />
                 </Grid>
             </Grid>
-            <Grid item xs={12}>
-                <Typography variant='h1' className='title'>{content.greeting}</Typography>
-                <Typography className='text'>{content.intro}</Typography>
-                <Typography className='text'>{content.primary}</Typography>
-                <Typography className='text'>{content.secondary}</Typography>
-                <Typography className='text'>{content.thanks}</Typography>
+            <Grid item xs={12} className='content-section-2'>
+                <Grid item xs={12} className='sub-section'>
+                    <Typography variant='h1' className='title'>{content2.greeting}</Typography>
+                    <Typography className='text'>{content2.intro}</Typography>
+                    <Typography className='text'>{content2.primary}</Typography>
+                    <Typography className='text'>{content2.secondary}</Typography>
+                    <Typography className='text'>{content2.thanks}</Typography>
+                </Grid>
             </Grid>
         </Grid>
     );
